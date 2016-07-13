88d4a44ffb035b88a39f84b8128f1feb9abf4fcc
Stanislav Stepanov <sstepanov@articul.office>
2016-07-13 12:31:12 +0300

    new fixes

--- lib/helpers/Debug.php
+++ lib/helpers/Debug.php
@@ -7,6 +7,7 @@ ini_set('error_log', $_SERVER['DOCUMENT_ROOT'].'/manual-errors.log');
  */
 class Debug
 {
+    private static $timestamp;
     /**
      * Shorten for '<pre>'.print_r($var, true).'</pre>'
      *
@@ -26,5 +27,28 @@ class Debug
     public static function logVar($var){
         error_log(print_r($var,true));
         return true;
+    }
+
+    /**
+     * Initializing or updates timestamp for logging time points.
+     */
+    public static function updateTimestamp(){
+        static::$timestamp = microtime(true);
+    }
+
+    /**
+     * @return mixed - current timestamp minus reserved.
+     */
+    public static function getTimestampDiff(){
+        return (microtime(true) - static::$timestamp);
+    }
+
+    /**
+     * @return mixed - returns current timestamp minus reserved then updates timestamp to current.
+     */
+    public static function getTimestampDiffUpdate(){
+        $diff = microtime(true) - static::$timestamp;
+        static::updateTimestamp();
+        return $diff;
     }
 }
